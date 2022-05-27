1f73471ec3f037015f4db65d62f7b28de7a1432f
lyjguy <lyjguy@naver.com>
2022-05-28 00:35:41 +0900

    spring + kotlin + jpa project init

---
+++ SpringKotlinDataJpa/src/main/kotlin/com/lyjguy/kotlin/springkotlindatajpa/SpringKotlinDataJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.lyjguy.kotlin.springkotlindatajpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinDataJpaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinDataJpaApplication>(*args)
+}
---
+++ SpringKotlinDataJpa/src/test/kotlin/com/lyjguy/kotlin/springkotlindatajpa/SpringKotlinDataJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lyjguy.kotlin.springkotlindatajpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinDataJpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
